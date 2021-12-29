357c812bc100589e7090160a98f1a46205f70504
BoiseState Student <student@u.boisestate.edu>
2021-12-29 12:42:54 -0700

    Added stub

---
+++ MASHGame/MASHGame.java
@@ -0,0 +1,12 @@
+/**
+ * TODO: Add description here
+ * @author Your Name
+ */
+
+ public class MASHGame
+ {
+     public static void main(String[] args)
+     {
+         // TODO: Implement code here
+     }
+ }
